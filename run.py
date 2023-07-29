2f48a049b01a5efba42dc9068547e660bf319d25
Daemon2017 <Daemon2017@gmail.com>
2023-07-29 15:51:47 +0300

    Добавлены проверки

--- run.py
+++ run.py
@@ -18,11 +18,21 @@ def request_txt():
     print(f'Received RQ request_txt: {request_id}')
     splitted_rows = split_rows(request.data)
     modal_markers_count = get_modal_markers_count(splitted_rows)
-    replaced_rows = replace_rows(splitted_rows, modal_markers_count)
-    Thread(target=process_txt,
-           args=(request_id, replaced_rows)).start()
-    return Response(json.dumps(dict(requestId=request_id)),
-                    mimetype='application/json')
+    if modal_markers_count <= 111:
+        haplotypes_count = get_haplotypes_count(splitted_rows)
+        if haplotypes_count > 1:
+            replaced_rows = replace_rows(splitted_rows, modal_markers_count)
+            Thread(target=process_txt,
+                   args=(request_id, replaced_rows)).start()
+            return Response(json.dumps(dict(requestId=request_id)),
+                            mimetype='application/json')
+        else:
+            return Response(json.dumps(dict(error="In the set, in addition to the modal, "
+                                                  "there must be more than 1 haplotype!")),
+                            mimetype='application/json')
+    else:
+        return Response(json.dumps(dict(error="A modal haplotype cannot have more than 111 markers!")),
+                        mimetype='application/json')
 
 
 @app.route('/request_png', methods=['POST'])
@@ -31,12 +41,21 @@ def request_png():
     print(f'Received RQ request_png: {request_id}')
     splitted_rows = split_rows(request.data)
     modal_markers_count = get_modal_markers_count(splitted_rows)
-    haplotypes_count = get_haplotypes_count(splitted_rows)
-    replaced_rows = replace_rows(splitted_rows, modal_markers_count)
-    Thread(target=process_png,
-           args=(request_id, replaced_rows, request.headers, modal_markers_count, haplotypes_count)).start()
-    return Response(json.dumps(dict(requestId=request_id)),
-                    mimetype='application/json')
+    if modal_markers_count <= 111:
+        haplotypes_count = get_haplotypes_count(splitted_rows)
+        if haplotypes_count > 1:
+            replaced_rows = replace_rows(splitted_rows, modal_markers_count)
+            Thread(target=process_png,
+                   args=(request_id, replaced_rows, request.headers, modal_markers_count, haplotypes_count)).start()
+            return Response(json.dumps(dict(requestId=request_id)),
+                            mimetype='application/json')
+        else:
+            return Response(json.dumps(dict(error="In the set, in addition to the modal, "
+                                                  "there must be more than 1 haplotype!")),
+                            mimetype='application/json')
+    else:
+        return Response(json.dumps(dict(error="A modal haplotype cannot have more than 111 markers!")),
+                        mimetype='application/json')
 
 
 @app.route('/request_pdf', methods=['POST'])
@@ -45,12 +64,21 @@ def request_pdf():
     print(f'Received RQ request_pdf: {request_id}')
     splitted_rows = split_rows(request.data)
     modal_markers_count = get_modal_markers_count(splitted_rows)
-    haplotypes_count = get_haplotypes_count(splitted_rows)
-    replaced_rows = replace_rows(splitted_rows, modal_markers_count)
-    Thread(target=process_pdf,
-           args=(request_id, replaced_rows, request.headers, modal_markers_count, haplotypes_count)).start()
-    return Response(json.dumps(dict(requestId=request_id)),
-                    mimetype='application/json')
+    if modal_markers_count <= 111:
+        haplotypes_count = get_haplotypes_count(splitted_rows)
+        if haplotypes_count > 1:
+            replaced_rows = replace_rows(splitted_rows, modal_markers_count)
+            Thread(target=process_pdf,
+                   args=(request_id, replaced_rows, request.headers, modal_markers_count, haplotypes_count)).start()
+            return Response(json.dumps(dict(requestId=request_id)),
+                            mimetype='application/json')
+        else:
+            return Response(json.dumps(dict(error="In the set, in addition to the modal, "
+                                                  "there must be more than 1 haplotype!")),
+                            mimetype='application/json')
+    else:
+        return Response(json.dumps(dict(error="A modal haplotype cannot have more than 111 markers!")),
+                        mimetype='application/json')
 
 
 @app.route('/response', methods=['GET'])
