8674c711a98117316a37022329114c349d24ed3c
Daemon2017 <Daemon2017@gmail.com>
2023-07-31 00:25:53 +0300

    Исправлен установщик

--- install.ps1
+++ install.ps1
@@ -13,11 +13,12 @@ Expand-Archive "python-3.10.11-embed-win32.zip" -DestinationPath "python"
 Remove-Item -Path "./python-3.10.11-embed-win32.zip"
 Invoke-WebRequest "https://bootstrap.pypa.io/get-pip.py" -OutFile "./python/get-pip.py"
 ./python/python.exe ./python/get-pip.py
-$content=@'
+$content=@"
 python310.zip
 .
 Lib
 Lib\site-packages
-'@
+$(Get-Location)
+"@
 New-Item -ItemType File -Path ./python/python310._pth -Value $content -Force
 ./python/python.exe -m pip install -r requirements.txt
