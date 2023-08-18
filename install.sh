de85af29202a2d8d9a14b1dd20186dd2f71e19c8
Daemon2017 <Daemon2017@gmail.com>
2023-08-18 11:12:49 +0300

    Скрипт доработан

--- install.sh
+++ install.sh
@@ -10,9 +10,16 @@ apt-get install python3.8 -y
 apt-get install python3-pip -y
 python3.8 -m pip install -r requirements.txt
 wget -O murka-1.4.1-src.tar.gz https://master.dl.sourceforge.net/project/phylomurka/murka/murka-1.4.1/murka-1.4.1-src.tar.gz?viasf=1
+rm -rf murka
 tar -zxvf murka-1.4.1-src.tar.gz
 rm murka-1.4.1-src.tar.gz
 mv murka-1.4.1-src murka
 cd murka || exit
 ./configure
 make
+cd murka || exit
+g++ -w -O2 -g0 -ldl -pthread -s -o murka murka.o nwerr.o args.o getopt.o misc.o nwclient_s.o nwclient_l.o nwclient.o nwst.o fformat.o
+cd ..
+make
+cd prepare || exit
+g++ -w -O2 -g0 -ldl -pthread -s ../murka/misc.o ../murka/getopt.o -o prepare prepare.o preperr.o prepargs.o convert.o
