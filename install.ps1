338bd7ab5f7b05f4efa8d8f6f3bd080f9eb6896b
Daemon2017 <Daemon2017@gmail.com>
2023-08-19 20:05:38 +0300

    Обновлен скрипт

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 Invoke-WebRequest "https://master.dl.sourceforge.net/project/phylomurka/murka/murka-1.4.1/murka-1.4.1-win64.zip?viasf=1" -OutFile "murka-1.4.1-win64.zip"
 Expand-Archive "murka-1.4.1-win64.zip" -DestinationPath "murka-1.4.1-win64"
+Remove-Item -Path "./murka"
 Move-Item -Path "./murka-1.4.1-win64/murka-1.4.1-win64" -Destination "./murka" -force
 Remove-Item -Path "./murka-1.4.1-win64.zip"
 Remove-Item -Path "./murka-1.4.1-win64"
